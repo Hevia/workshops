0995a51881da1bf72a9d3abe958124207ce0b579
Anthony Hevia <anthonyhevia@microsoft.com>
2021-04-09 00:11:54 -0400

    making all the web api's

---
+++ carbon_efficent_rust/backend/python_backend_app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+#uvicorn main:app --reload
+import time
+from typing import Optional
+
+app = FastAPI()
+
+@app.get("/")
+def main_route(name: Optional[str] = None):
+    time.sleep(2)
+    return name
